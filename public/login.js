ce549ec3c66f9d402dc36e24de4e109ceea5854a
Mauri <nestorjesus563@gmail.com>
2026-05-24 09:43:20 -0600

    Arreglar conflicto en opciones.html

--- public/login.js
+++ public/login.js
@@ -1,30 +1,17 @@
 function login(){
 
     let usuarioAprobado = "allis";
-
     let contraAprobada = "1234";
 
     let usuario = document.getElementById("usuario").value;
-
     let contrasena = document.getElementById("contrasena").value;
 
     if(
-
         usuario == usuarioAprobado &&
-
         contrasena == contraAprobada
-
     ){
-
         window.location.href = "opciones.html";
-
     }else{
-
         alert("Usuario o contraseña incorrectos");
-
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 62637ac (Corregir rutas para Render)
