3193700f5064f0e11107bfd1fd35672401f54eaf
Mauri <nestorjesus563@gmail.com>
2026-05-23 18:00:50 -0600

    mysql online

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.static("public"));
 
 const conexion = mysql.createConnection({
 
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "allicooklyn"
+    host: process.env.DB_HOST,
+
+    user: process.env.DB_USER,
+
+    password: process.env.DB_PASSWORD,
+
+    database: process.env.DB_NAME,
+
+    port: process.env.DB_PORT
 });
 
 conexion.connect((error) => {
